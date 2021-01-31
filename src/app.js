6a126e96a827a016dbf3426f8a8f51751c82e4fb
Devodriq Roberts <devodriq.roberts@leasequery.com>
2021-01-30 21:33:06 -0500

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const forecast = require("./utils/forecast")
 
 // Create the app with the express call.
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config.
 const publicDirPath = path.join(__dirname, '../public')
@@ -101,6 +102,6 @@ app.get("*", (req, res) => {
 
 
 // Start the express server.
-app.listen(3000, () => {
-    console.log("Server is up on port 3000")
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
 })
